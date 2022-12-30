966c89c3973adf0267e1e11c51ccc3e9bf977aaf
igd-revature <gangadhar.parde@revature.com>
2022-12-30 16:50:34 +0530

    added code

--- src/com/revature/app/MenuDrivenApplication.java
+++ src/com/revature/app/MenuDrivenApplication.java
@@ -1,7 +1,61 @@
 package com.revature.app;
 
+//import ---> statement use to refer the java API from another package not present in current packages.
+import java.util.Scanner;
+
+import com.revature.constants.Constant;
+
 public class MenuDrivenApplication {
 	public static void main(String[] args) {
-		
+		howToGetValueEasilyFromUser();
+	}
+
+	private static void howToGetValueEasilyFromUser() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Constant.HEADER_MSG);
+		char proceed = 'Y';
+		do {
+			System.out.println(Constant.FINAL_MENU_MSG);
+			System.out.print(Constant.MENU_CHOICE_MSG);
+			int choice = sc.nextInt();
+
+			if (choice == 1) {
+				registerUser(sc);
+			} else {
+				break;
+			}
+
+			System.out.print(Constant.MENU_CONTINUE_MSG);
+			proceed = sc.next().toUpperCase().charAt(0);
+			// System.out.println(proceed!='N');
+			// System.out.println(proceed!='n');
+			// System.out.println(proceed!='N' || proceed!='n');
+
+		} while (proceed != 'N');
+		System.out.println(Constant.FOOTER_MSG);
+	}
+
+	private static void registerUser(Scanner sc) {
+		System.out.print(Constant.MSG_ENTER_AGE_HINDI);
+		int age = sc.nextInt();
+		System.out.print(Constant.MSG_ENTER_NAME_HINDI);
+		String name = sc.next();
+		System.out.printf(Constant.SUCESSFUL_REGISTRATION_MESSAGE, name, age);
 	}
 }
+
+// १८   
+// गंगाधर
+
+/*
+ * Ctrl+Shift+F format code Ctrl+shift+/ --- comment multi line comment
+ * Ctrl+shift+\ --- uncomment comment multi line comment Ctrl_+/ -- comment
+ * single line comment Ctrl_+\ -- uncomment single line comment
+ * 
+ * 
+ * -- Menu Driven Program -- Foundational Project P0 Accept simple value from
+ * user? What is I/O? We use java.io package accept user input from keyboard. :
+ * System.in ------ > Keyboard. Writing the data to files is the output
+ * operation. : System.out ---> Send o/p to console. Error can be send to log
+ * files System.err
+ */
--- src/com/revature/constants/Constant.java
+++ src/com/revature/constants/Constant.java
@@ -1,6 +1,24 @@
 package com.revature.constants;
 
 public class Constant {
-	public static double PIE_VALUE=3.14f;
-	public static String ENERGY_EQUATION="E=mc2";
+	public static double PIE_VALUE = 3.14f;
+	public static String ENERGY_EQUATION = "E=mc2";
+	
+	public static String MENU_MSG = "---- How can I help? ----";
+	public static String MENU1_MSG = "\t1. Sign Up";
+	public static String MENU2_MSG = "\t2. Exit";
+	public static String FINAL_MENU_MSG=MENU_MSG + "\n" + MENU1_MSG + "\n" + MENU2_MSG + "\n";
+	public static String MENU_CHOICE_MSG = "Enter choice:";
+	public static String MENU_CONTINUE_MSG = "Do you want to continue? (Y/N):";
+	
+	public static String HEADER_MSG = "###### नमस्ते ! ######";
+	public static String MSG_ENTER_AGE_ENGLISH = "Please enter your age:";
+	public static String MSG_ENTER_NAME_HINDI = "कृपया अपना नाम दर्ज करें:- ";
+	public static String MSG_ENTER_AGE_HINDI = "कृपया अपनी उम्र दर्ज करें:- ";
+	public static String SHOW_AGE_ENGLISH = "Age: ";
+	public static String SHOW_AGE_MSG_HINDI = "आपकी उम्र ";
+	public static String SHOW_NAME_MSG__HINDI = "आपक नाम";
+	public static String SUCESSFUL_REGISTRATION_MESSAGE="आपके द्वारा प्रस्तुत %s नाम और %d उम्र सफलतापूर्वक दर्ज किए गए हैं.\n\n";
+	public static String FOOTER_MSG = "\n\nआपका दिन शुभ रहे, धन्यवाद!\n###### सर्वाधिकार सुरक्षित | रेवेचर इंडिया प्राइवेट लिमिटेड © \u0968\u0966\u0968\u0968 ###### ";
+	
 }
